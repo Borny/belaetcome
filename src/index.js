4a63dabfc58cb075eb946593f24317ba4a2fedee
tristan <borntowebb@gmail.com>
2020-03-27 01:13:35 +0100

    Styled the contact page

--- src/index.js
+++ src/index.js
@@ -19,44 +19,41 @@ mobileNavOverlay.addEventListener('click', toggleMobileNav);
 mobileNavLinks.forEach(link => {
   link.addEventListener('click', toggleMobileNav)
 })
+
 /////////
 // FORM
 /////////
 
 // INPUT LABEL ANIMATION
 // Gets all the input_field elements of the page
-const inputFields = document.querySelectorAll('.input_field');
+const inputFields = document.querySelectorAll('.form__control');
 const submitContactBtn = document.getElementById('submit-contact-btn');
 
 const focusOnInput = (input) => {
-  input.target.value !== '' ?
-    input.target.parentNode.classList.add('filled') :
-    input.target.parentNode.classList.remove('filled')
+  if (input) {
+    input.target.value !== '' ?
+      input.target.parentNode.classList.add('filled') :
+      input.target.parentNode.classList.remove('filled')
+  }
 }
 
-// Goes through the array inputField
+// Goes through the array inputFields
 inputFields.forEach(input => {
   // Creates an event listener on the current input_field
   input.addEventListener('input', focusOnInput.bind(this));
-});
+})
+
+// Clears the inputs on page load
+const clearInputs = () => {
+  inputFields.forEach(input => {
+    input.value = '';
+  })
+}
+
+clearInputs();
 
 // SUBMIT BUTTON
 submitContactBtn.addEventListener('click', () => {
   console.log('button clicked');
   alert('you sure you wanna do this???')
 })
-
-// const toggleBtn = document.getElementById('toggle');
-// let disabled;
-
-// const toggleBtnHandler = () => {
-//   console.log('toggle')
-//   disabled = submitContactBtn.classList.contains('btn--disabled')
-
-//   disabled
-//     ?
-//     submitContactBtn.classList.remove('btn--disabled') :
-//     submitContactBtn.classList.add('btn--disabled');
-// }
-
-// toggleBtn.addEventListener('click', toggleBtnHandler)
