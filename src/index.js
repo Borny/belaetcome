019964e7f251e1f6267694ebfa664a1e994ea64d
tristan <borntowebb@gmail.com>
2020-03-29 22:14:56 +0200

    Added loader to the main page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import './styles/main.scss';
 
+window.onload = function () {
+  document.getElementById("loader").style.opacity = 0
+  document.getElementById('loader-path').style.opacity = 0;
+  setTimeout(() => {
+    document.getElementById("loader").style.display = "none"
+  }, 2000)
+}
+
 const body = document.getElementsByTagName('body')[0];
 
 //////////////////////
