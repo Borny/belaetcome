a4a84c974e440cbf4ad0e2462b07e9c8734d3647
tristan <borntowebb@gmail.com>
2020-04-18 13:21:29 +0200

    Fixed the list in the residence options text

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,11 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  devServer: {
+    hot: true,
+    http2: true,
+    https: true
+  },
   module: {
     rules: [{
         test: /\.html$/,
